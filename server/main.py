65606eda1addbc3b7d75ef8f45443d0540a2b80c
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-01-26 08:59:34 +0800

    Added Flask app, config, and ping route. Error with sqlite found where it cannot find db again as I moved the db into a separate folder

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
 from sqlalchemy import Column, DateTime, Integer, String, create_engine 
 from sqlalchemy.orm import declarative_base, sessionmaker
-from datetime import datetime
+from datetime import datetime, timedelta
 
-engine = create_engine('sqlite:///server/database.db')
+engine = create_engine('sqlite:///server/db/database.db')
 Base = declarative_base()
 
 class AuthUser(Base):
@@ -22,3 +24,18 @@ print("Session created!")
 
 query_auth_users = session.query(AuthUser)
 print(query_auth_users)
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=360)
+
+CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+
+app.route('/ping', methods=['GET', 'POST'])
+def index():
+
+    return {"message": "pong!"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5821)
