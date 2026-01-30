b0ccb09f522ff3cc3ef872fb1ab18005c22ef07d
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-01-30 14:50:34 +0800

    Removed placeholder code from Nextjs and added a /portal route where it would serve as a main module for the time being

---
+++ web/src/app/portal/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-slate-200">
+      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-slate-100 sm:items-start">
+        
+      </main>
+    </div>
+  );
+}
