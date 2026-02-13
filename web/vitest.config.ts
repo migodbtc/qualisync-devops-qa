885e5c381a847a627a83e76d93c30b50fb609ddc
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-02-13 14:31:04 +0800

    Added first test for pytest, a basic sanity check to see if it works

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-
-        // instead of importing vitest functions one-by-one
-        // just set this to true and by default, vitest keywords
-        // dont need any import
-        globals: true, 
-
         // for the mock dom to be used for testing
         // jsdom will be the mock dom
         setupFiles: ['./src/tests/setup.ts'],
         environment: 'jsdom',
+        globals: true,
     }
 });
