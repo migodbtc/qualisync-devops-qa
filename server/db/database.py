72c894b744375a478742819191ebfa051a37eb64
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-02-28 15:08:31 +0800

    Refactor Taskfile and Docker configurations; add MySQL service to docker-compose; update database connection settings; enhance .gitignore and environment files for better management

--- server/db/database.py
+++ server/db/database.py
@@ -1,6 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root:@localhost/fuchsia_atms_db")
+# configs for sql engine
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "very_secure_password_for_qualisync")
+DB_NAME = os.getenv("DB_NAME", "fuchsia_atms_db")
+
+# engine string
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 db = SessionLocal()
